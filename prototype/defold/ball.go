189f5adf3999ff5200a423b78ebfb49d4e6879c3
Plicerin <48420902+Plicerin@users.noreply.github.com>
2026-04-24 16:18:53 -0400

    Add Defold game objects, factories, and collection stub

---
+++ prototype/defold/ball.go
@@ -0,0 +1,13 @@
+embedded_components:
+- id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/placeholder.atlas\"\ndefault_animation: \"placeholder\"\nmaterial: \"/builtins/materials/default.material\"\nblend_mode: alpha\n"
+  position: 0.0, 0.0, 0.0
+  rotation: 0.0, 0.0, 0.0, 1.0
+
+embedded_components:
+- id: "script"
+  type: "script"
+  data: "script: \"/scripts/ball.script\"\n"
+  position: 0.0, 0.0, 0.0
+  rotation: 0.0, 0.0, 0.0, 1.0
