fefc7ae174503382d7b5d8db0b222552a348fef1
Plicerin <48420902+Plicerin@users.noreply.github.com>
2026-04-24 18:01:00 -0400

    Add valid spawner.go (proper Defold .go format) and remove .stub

---
+++ prototype/defold/spawner.go
@@ -0,0 +1,67 @@
+embedded_components {
+  id: "player_factory"
+  type: "factory"
+  data: "prototype: \"/player_factory.factory\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+
+embedded_components {
+  id: "ai_player_factory"
+  type: "factory"
+  data: "prototype: \"/ai_player_factory.factory\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+
+embedded_components {
+  id: "ball_factory"
+  type: "factory"
+  data: "prototype: \"/ball_factory.factory\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+
+embedded_components {
+  id: "script"
+  type: "script"
+  data: "script: \"/scripts/spawner.script\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
